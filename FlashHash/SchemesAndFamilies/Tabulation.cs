8626e18dd42271514be0e6e1bdceb683a460d6fb
Vojtech Gadurek <vg.vojtawar@outlook.com>
2024-06-16 00:39:22 +0200

    add tabulation

---
+++ FlashHash/SchemesAndFamilies/Tabulation.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlashHash.SchemesAndFamilies
+{
+	public class TabulationFamily : IHashingFunctionFamily<TabulationScheme>
+	{
+		static Random _random = new Random();
+		public TabulationScheme GetScheme(ulong size, ulong offset)
+		{
+			return new TabulationScheme(GetTable(), size, offset);
+		}
+
+		public static ulong[][] GetTable()
+		{
+			ulong[][] table = new ulong[8][];
+			for (int i = 0; i < 8; i++)
+			{
+				table[i] = new ulong[256];
+
+				var t = table[i];
+				for (int j = 0; j < 256; j++)
+				{
+					t[j] = (ulong)_random.NextInt64();
+				}
+			}
+			return table;
+
+		}
+
+		public void SetRandomness(Random random)
+		{
+			_random = random;
+		}
+
+		IHashingFunctionScheme IHashingFunctionFamily.GetScheme(ulong size, ulong offset)
+		{
+			return GetScheme(size, offset);
+		}
+	}
+
+	public record struct TabulationScheme(ulong[][] Table, ulong Size, ulong Offset) : IHashingFunctionScheme
+	{
+		public Expression<HashingFunction> Create()
+		{
+			const int length = 8;
+			var f = CompiledFunctions.Create<ulong, ulong>(out var input_);
+			f.S.DeclareVariable<int>(out var i_, 0)
+				.DeclareVariable<ulong[][]>(out var table_, Table)
+				.While(i_.V < length,
+					new Scope().DeclareVariable<int>(out var index_, input_.V.Convert<int>() % (1 << length))
+					.Assign(f.Output, f.Output.V ^ table_.V.ToTable<ulong[]>()[i_.V].V.ToTable<ulong>()[index_.V].V))
+				);
+			return f.Construct();
+		}
+
+		public bool Equals(IHashingFunctionScheme? other)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
