7bafcdd37e24f826a24dd9faff3385dfacdf774f
Vojtech Gadurek <vg.vojtawar@outlook.com>
2025-01-01 18:51:47 +0100

    fix naming

--- FlashHashBenchmarks/ExplicitDelegateExpressionTreesHashingFunctionBenchmark.cs
+++ FlashHashBenchmarks/ExplicitDelegateExpressionTreesHashingFunctionBenchmark.cs
@@ -9,213 +9,213 @@ using System.Threading.Tasks;
 
 namespace FlashHashBenchmarks
 {
-	public class ExplicitDelegateExpressionTreesHashingFunctionBenchmark
-	{
-		public const int Length = 1024;
-		public const int BufferLength = 4096;
-		public const int DataLength = Length * BufferLength;
-		public Random Random;
-		public RandomData Stream;
+    public class ExplicitDelegateExpressionTreesHashingFunctionBenchmark
+    {
+        public const int Length = 1024;
+        public const int BufferLength = 4096;
+        public const int DataLength = Length * BufferLength;
+        public Random Random;
+        public RandomData Stream;
 
-		[GlobalSetup]
-		public void GlobalSetup()
-		{
-			Random = new Random();
-			Stream = new RandomData((int)DataLength, Random);
-		}
+        [GlobalSetup]
+        public void GlobalSetup()
+        {
+            Random = new Random();
+            Stream = new RandomData((int)DataLength, Random);
+        }
 
-		[IterationSetup]
-		public void IterationSetup()
-		{
-			Stream.Reset();
-		}
+        [IterationSetup]
+        public void IterationSetup()
+        {
+            Stream.Reset();
+        }
 
-		[Benchmark]
-		public ulong LinearCongruenceExpressionTreesBetterBuffered()
-		{
-			ulong a = (ulong)Random!.NextInt64();
-			ulong b = (ulong)Random.NextInt64();
-			ulong size = (ulong)Random.NextInt64(0, 2 << 60);
-			ulong prime = LinearCongruenceScheme.GetGoodPrime(size);
+        [Benchmark]
+        public ulong LinearCongruenceExpressionTreesBetterBuffered()
+        {
+            ulong a = (ulong)Random!.NextInt64();
+            ulong b = (ulong)Random.NextInt64();
+            ulong size = (ulong)Random.NextInt64(0, 2 << 60);
+            ulong prime = LinearCongruenceScheme.GetGoodPrime(size);
 
-			var action = CompiledActions.Create<ulong[], ulong[], int, int>
-				(out var input_, out var output_, out var start_, out var nItems_);
-			action.S.DeclareVariable<int>(out var i_, 0)
-			.Macro(out var input_T, input_.V.ToTable<ulong>())
-			.Macro(out var output_T, output_.V.ToTable<ulong>())
-			.While(i_.V < nItems_.V, new Scope().Assign(output_T[start_.V + i_.V],
-			(input_T[start_.V + i_.V].V * a + b) % prime % size).Assign(i_, i_.V + 1));
-			var f = action.Construct().Compile();
+            var action = CompiledActions.Create<ulong[], ulong[], int, int>
+                (out var input_, out var output_, out var start_, out var nItems_);
+            action.S.DeclareVariable<int>(out var i_, 0)
+            .Macro(out var input_T, input_.V.ToTable<ulong>())
+            .Macro(out var output_T, output_.V.ToTable<ulong>())
+            .While(i_.V < nItems_.V, new Scope().Assign(output_T[start_.V + i_.V],
+            (input_T[start_.V + i_.V].V * a + b) % prime % size).Assign(i_, i_.V + 1));
+            var f = action.Construct().Compile();
 
-			ulong sum = 0;
-			ulong[] buffer = new ulong[BufferLength];
-			ulong[] answerBuffer = new ulong[BufferLength];
+            ulong sum = 0;
+            ulong[] buffer = new ulong[BufferLength];
+            ulong[] answerBuffer = new ulong[BufferLength];
 
-			while (Stream!.FillBuffer(buffer) > 0)
-			{
-				f(buffer, answerBuffer, 0, BufferLength);
+            while (Stream!.FillBuffer(buffer) > 0)
+            {
+                f(buffer, answerBuffer, 0, BufferLength);
 
-				for (int j = 0; j < BufferLength; j++)
-				{
-					sum += answerBuffer[j];
-				}
-			}
-			return sum;
+                for (int j = 0; j < BufferLength; j++)
+                {
+                    sum += answerBuffer[j];
+                }
+            }
+            return sum;
 
-		}
-
-
-		[Benchmark]
-		public ulong LinearCongruenceExpressionTreesBuffered()
-		{
-			ulong sum = 0;
-			ulong[] buffer = new ulong[BufferLength];
-			ulong[] answerBuffer = new ulong[BufferLength];
-			Action<ulong[], ulong[], int, int> f = LittleSharp.Utils.Buffering.BufferFunction(
-				HashingFunctionProvider.Get(typeof(LinearCongruenceFamily), (ulong)Random.NextInt64(0, 2 << 60), 0).Create()).Compile(); ;
-
-			while (Stream!.FillBuffer(buffer) > 0)
-			{
-				f(buffer, answerBuffer, 0, BufferLength);
-
-				for (int j = 0; j < BufferLength; j++)
-				{
-					sum += answerBuffer[j];
-				}
-			}
-			return sum;
-		}
-
-		[Benchmark]
-		public ulong LinearCongruenceExpressionTrees()
-		{
-			ulong sum = 0;
-			ulong[] buffer = new ulong[BufferLength];
-			ulong[] answerBuffer = new ulong[BufferLength];
-			Func<ulong, ulong> f =
-				HashingFunctionProvider.Get(typeof(LinearCongruenceFamily), (ulong)Random.NextInt64(0, 2 << 60), 0).Create().Compile();
-
-			while (Stream!.FillBuffer(buffer) > 0)
-			{
-				for (int j = 0; j < BufferLength; j++)
-				{
-					sum += f(buffer[j]);
-				}
-
-			}
-			return sum;
-		}
+        }
 
 
-		[Benchmark(Baseline = true)]
-		public ulong LinearCongruenceDelegateBuffered()
-		{
-			ulong a = (ulong)Random!.NextInt64();
-			ulong b = (ulong)Random.NextInt64();
-			ulong size = (ulong)Random.NextInt64(0, 2 << 60);
-			ulong prime = LinearCongruenceScheme.GetGoodPrime(size);
-			Action<ulong[], ulong[], int, int> f = (input, output, start, length) =>
-			{
-				for (int i = 0; i < length; i++)
-				{
-					output[start + i] = (input[start + i] * a + b) % prime % size;
-				}
-			};
+        [Benchmark]
+        public ulong LinearCongruenceExpressionTreesBuffered()
+        {
+            ulong sum = 0;
+            ulong[] buffer = new ulong[BufferLength];
+            ulong[] answerBuffer = new ulong[BufferLength];
+            Action<ulong[], ulong[], int, int> f = LittleSharp.Utils.Buffering.BufferFunction(
+                HashFunctionProvider.Get(typeof(LinearCongruenceFamily), (ulong)Random.NextInt64(0, 2 << 60), 0).Create()).Compile(); ;
+
+            while (Stream!.FillBuffer(buffer) > 0)
+            {
+                f(buffer, answerBuffer, 0, BufferLength);
+
+                for (int j = 0; j < BufferLength; j++)
+                {
+                    sum += answerBuffer[j];
+                }
+            }
+            return sum;
+        }
+
+        [Benchmark]
+        public ulong LinearCongruenceExpressionTrees()
+        {
+            ulong sum = 0;
+            ulong[] buffer = new ulong[BufferLength];
+            ulong[] answerBuffer = new ulong[BufferLength];
+            Func<ulong, ulong> f =
+                HashFunctionProvider.Get(typeof(LinearCongruenceFamily), (ulong)Random.NextInt64(0, 2 << 60), 0).Create().Compile();
+
+            while (Stream!.FillBuffer(buffer) > 0)
+            {
+                for (int j = 0; j < BufferLength; j++)
+                {
+                    sum += f(buffer[j]);
+                }
+
+            }
+            return sum;
+        }
 
 
-			ulong sum = 0;
-			ulong[] buffer = new ulong[BufferLength];
-			ulong[] answerBuffer = new ulong[BufferLength];
-
-			while (Stream!.FillBuffer(buffer) > 0)
-			{
-				f(buffer, answerBuffer, 0, BufferLength);
-
-				for (int j = 0; j < BufferLength; j++)
-				{
-					sum += answerBuffer[j];
-				}
-			}
-			return sum;
-		}
-
-		[Benchmark]
-		public ulong LinearCongruenceDelegate()
-		{
-			ulong sum = 0;
-			ulong a = (ulong)Random!.NextInt64();
-			ulong b = (ulong)Random.NextInt64();
-			ulong size = (ulong)Random.NextInt64(0, 2 << 60);
-			ulong prime = LinearCongruenceScheme.GetGoodPrime(size);
-			ulong[] buffer = new ulong[BufferLength];
-			ulong[] answerBuffer = new ulong[BufferLength];
-			Func<ulong, ulong> f = (input) => (input * a + b) % prime % size;
-
-			while (Stream!.FillBuffer(buffer) > 0)
-			{
-				for (int j = 0; j < BufferLength; j++)
-				{
-					sum += f(buffer[j]);
-				}
-
-			}
-			return sum;
-		}
-
-		[Benchmark]
-
-		public ulong LinearCongruenceExplicitBuffered()
-		{
-			ulong a = (ulong)Random!.NextInt64();
-			ulong b = (ulong)Random.NextInt64();
-			ulong size = (ulong)Random.NextInt64(0, 2 << 60);
-			ulong prime = LinearCongruenceScheme.GetGoodPrime(size);
-			Action<ulong[], ulong[], int, int> f = (input, output, start, length) =>
-			{
-				for (int i = 0; i < length; i++)
-				{
-					output[start + i] = (input[start + i] * 122032421 + 129934) % 18338 % 3400;
-				}
-			};
+        [Benchmark(Baseline = true)]
+        public ulong LinearCongruenceDelegateBuffered()
+        {
+            ulong a = (ulong)Random!.NextInt64();
+            ulong b = (ulong)Random.NextInt64();
+            ulong size = (ulong)Random.NextInt64(0, 2 << 60);
+            ulong prime = LinearCongruenceScheme.GetGoodPrime(size);
+            Action<ulong[], ulong[], int, int> f = (input, output, start, length) =>
+            {
+                for (int i = 0; i < length; i++)
+                {
+                    output[start + i] = (input[start + i] * a + b) % prime % size;
+                }
+            };
 
 
-			ulong sum = 0;
-			ulong[] buffer = new ulong[BufferLength];
-			ulong[] answerBuffer = new ulong[BufferLength];
+            ulong sum = 0;
+            ulong[] buffer = new ulong[BufferLength];
+            ulong[] answerBuffer = new ulong[BufferLength];
 
-			while (Stream!.FillBuffer(buffer) > 0)
-			{
-				f(buffer, answerBuffer, 0, BufferLength);
+            while (Stream!.FillBuffer(buffer) > 0)
+            {
+                f(buffer, answerBuffer, 0, BufferLength);
 
-				for (int j = 0; j < BufferLength; j++)
-				{
-					sum += answerBuffer[j];
-				}
-			}
-			return sum;
-		}
-		[Benchmark]
-		public ulong LinearCongruenceExplicit()
-		{
-			ulong sum = 0;
-			ulong a = (ulong)Random!.NextInt64();
-			ulong b = (ulong)Random.NextInt64();
-			ulong size = (ulong)Random.NextInt64(0, 2 << 60);
-			ulong prime = LinearCongruenceScheme.GetGoodPrime(size);
-			ulong[] buffer = new ulong[BufferLength];
-			ulong[] answerBuffer = new ulong[BufferLength];
-			Func<ulong, ulong> f = (input) => (input * 122032421 + 129934) % 18338 % 3400;
+                for (int j = 0; j < BufferLength; j++)
+                {
+                    sum += answerBuffer[j];
+                }
+            }
+            return sum;
+        }
 
-			while (Stream!.FillBuffer(buffer) > 0)
-			{
-				for (int j = 0; j < BufferLength; j++)
-				{
-					sum += f(buffer[j]);
-				}
+        [Benchmark]
+        public ulong LinearCongruenceDelegate()
+        {
+            ulong sum = 0;
+            ulong a = (ulong)Random!.NextInt64();
+            ulong b = (ulong)Random.NextInt64();
+            ulong size = (ulong)Random.NextInt64(0, 2 << 60);
+            ulong prime = LinearCongruenceScheme.GetGoodPrime(size);
+            ulong[] buffer = new ulong[BufferLength];
+            ulong[] answerBuffer = new ulong[BufferLength];
+            Func<ulong, ulong> f = (input) => (input * a + b) % prime % size;
 
-			}
-			return sum;
-		}
+            while (Stream!.FillBuffer(buffer) > 0)
+            {
+                for (int j = 0; j < BufferLength; j++)
+                {
+                    sum += f(buffer[j]);
+                }
 
-	}
+            }
+            return sum;
+        }
+
+        [Benchmark]
+
+        public ulong LinearCongruenceExplicitBuffered()
+        {
+            ulong a = (ulong)Random!.NextInt64();
+            ulong b = (ulong)Random.NextInt64();
+            ulong size = (ulong)Random.NextInt64(0, 2 << 60);
+            ulong prime = LinearCongruenceScheme.GetGoodPrime(size);
+            Action<ulong[], ulong[], int, int> f = (input, output, start, length) =>
+            {
+                for (int i = 0; i < length; i++)
+                {
+                    output[start + i] = (input[start + i] * 122032421 + 129934) % 18338 % 3400;
+                }
+            };
+
+
+            ulong sum = 0;
+            ulong[] buffer = new ulong[BufferLength];
+            ulong[] answerBuffer = new ulong[BufferLength];
+
+            while (Stream!.FillBuffer(buffer) > 0)
+            {
+                f(buffer, answerBuffer, 0, BufferLength);
+
+                for (int j = 0; j < BufferLength; j++)
+                {
+                    sum += answerBuffer[j];
+                }
+            }
+            return sum;
+        }
+        [Benchmark]
+        public ulong LinearCongruenceExplicit()
+        {
+            ulong sum = 0;
+            ulong a = (ulong)Random!.NextInt64();
+            ulong b = (ulong)Random.NextInt64();
+            ulong size = (ulong)Random.NextInt64(0, 2 << 60);
+            ulong prime = LinearCongruenceScheme.GetGoodPrime(size);
+            ulong[] buffer = new ulong[BufferLength];
+            ulong[] answerBuffer = new ulong[BufferLength];
+            Func<ulong, ulong> f = (input) => (input * 122032421 + 129934) % 18338 % 3400;
+
+            while (Stream!.FillBuffer(buffer) > 0)
+            {
+                for (int j = 0; j < BufferLength; j++)
+                {
+                    sum += f(buffer[j]);
+                }
+
+            }
+            return sum;
+        }
+
+    }
 }
--- FlashHashTests/BasicTests.cs
+++ FlashHashTests/BasicTests.cs
@@ -3,102 +3,102 @@ using Xunit.Abstractions;
 
 namespace FlashHashTests
 {
-	namespace Tests
-	{
-		public class ModuloTest
-		{
-			[Fact]
-			public void TestModulo()
-			{
-				Random random = new Random(42);
-				for (ulong i = 0; i < 100; i++)
-				{
-					ulong randomSize = (ulong)random.NextInt64();
-					var modulo = new ModuloScheme(randomSize, 0).Create().Compile();
+    namespace Tests
+    {
+        public class ModuloTest
+        {
+            [Fact]
+            public void TestModulo()
+            {
+                Random random = new Random(42);
+                for (ulong i = 0; i < 100; i++)
+                {
+                    ulong randomSize = (ulong)random.NextInt64();
+                    var modulo = new ModuloScheme(randomSize, 0).Create().Compile();
 
-					ulong randomValue = (ulong)random.NextInt64();
-					Assert.Equal(modulo(randomValue), randomValue % randomSize);
-				}
-			}
+                    ulong randomValue = (ulong)random.NextInt64();
+                    Assert.Equal(modulo(randomValue), randomValue % randomSize);
+                }
+            }
 
-		}
+        }
 
-		public class MultiplyShiftTest
-		{
+        public class MultiplyShiftTest
+        {
 
-		}
+        }
 
-		public class TabulationHashing
-		{
-			[Fact]
-			void BasicTest()
-			{
-				ulong[][] table = new ulong[8][];
-				for (ulong i = 0; i < 8; i++)
-				{
-					table[i] = new ulong[256];
+        public class TabulationHashing
+        {
+            [Fact]
+            void BasicTest()
+            {
+                ulong[][] table = new ulong[8][];
+                for (ulong i = 0; i < 8; i++)
+                {
+                    table[i] = new ulong[256];
 
-					for (ulong j = 0; j < 256; j++)
-					{
-						table[i][j] = j << (int)(i * 8);
-					}
-				}
+                    for (ulong j = 0; j < 256; j++)
+                    {
+                        table[i][j] = j << (int)(i * 8);
+                    }
+                }
 
-				var m = Map.GetMap(Enumerable.Range(0, 256).Select(x => (ulong)x).Zip(table[0])).Compile();
+                var m = Map.GetMap(Enumerable.Range(0, 256).Select(x => (ulong)x).Zip(table[0])).Compile();
 
-				for (ulong i = 0; i < 256; i++) Assert.Equal(i, m(i));
-				const ulong size = 10;
+                for (ulong i = 0; i < 256; i++) Assert.Equal(i, m(i));
+                const ulong size = 10;
 
-				var tab = new TabulationScheme(table, size, 0);
-				var hf = tab.Create().Compile();
-
-
-				for (ulong i = 0; i < 256; i++) Assert.Equal(i % size, hf(i));
+                var tab = new TabulationScheme(table, size, 0);
+                var hf = tab.Create().Compile();
 
 
-				for (ulong i = 0; i < 1000; i++)
-				{
-					var ran = (ulong)Random.Shared.NextInt64();
-					Assert.Equal(ran % size, hf(ran));
-				}
-
-			}
+                for (ulong i = 0; i < 256; i++) Assert.Equal(i % size, hf(i));
 
 
-			public class HashingFunctionProviderTest
-			{
-				//output is a list of all hashing function families
+                for (ulong i = 0; i < 1000; i++)
+                {
+                    var ran = (ulong)Random.Shared.NextInt64();
+                    Assert.Equal(ran % size, hf(ran));
+                }
 
-				private readonly ITestOutputHelper _output;
-				public HashingFunctionProviderTest(ITestOutputHelper output)
-				{
-					_output = output;
-				}
-				[Fact]
-				public void TestGetAllHashingFunctionFamilies()
-				{
-					var families = HashingFunctionProvider.GetAllHashingFunctionFamilies();
-					foreach (var family in families)
-					{
-						_output.WriteLine(family.ToString());
-					}
-					Assert.NotEmpty(families);
-				}
+            }
 
-				[Fact]
-				public void TestGet()
-				{
-					var families = HashingFunctionProvider.GetAllHashingFunctionFamilies();
-					foreach (var family in families)
-					{
-						var scheme = HashingFunctionProvider.Get(family, 42, 0);
 
-						_output.WriteLine(scheme.ToString());
-						Assert.NotNull(scheme);
-					}
-				}
-			}
+            public class HashingFunctionProviderTest
+            {
+                //output is a list of all hashing function families
 
-		}
-	}
+                private readonly ITestOutputHelper _output;
+                public HashingFunctionProviderTest(ITestOutputHelper output)
+                {
+                    _output = output;
+                }
+                [Fact]
+                public void TestGetAllHashingFunctionFamilies()
+                {
+                    var families = HashFunctionProvider.GetAllHashingFunctionFamilies();
+                    foreach (var family in families)
+                    {
+                        _output.WriteLine(family.ToString());
+                    }
+                    Assert.NotEmpty(families);
+                }
+
+                [Fact]
+                public void TestGet()
+                {
+                    var families = HashFunctionProvider.GetAllHashingFunctionFamilies();
+                    foreach (var family in families)
+                    {
+                        var scheme = HashFunctionProvider.Get(family, 42, 0);
+
+                        _output.WriteLine(scheme.ToString());
+                        Assert.NotNull(scheme);
+                    }
+                }
+            }
+
+        }
+    }
 }
